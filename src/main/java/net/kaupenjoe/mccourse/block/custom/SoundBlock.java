64fa02fa6ea9dae605f62ebdec535012e07e6c51
ElRedstoniano <42276499+ElRedstoniano@users.noreply.github.com>
2024-07-06 00:01:36 +0100

    Descripciones custom de items - Tooltips

--- src/main/java/net/kaupenjoe/mccourse/block/custom/SoundBlock.java
+++ src/main/java/net/kaupenjoe/mccourse/block/custom/SoundBlock.java
@@ -3,16 +3,23 @@ package net.kaupenjoe.mccourse.block.custom;
 import net.kaupenjoe.mccourse.MCCourseMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Settings settings) {
@@ -59,5 +66,11 @@ public class SoundBlock extends Block {
                 SoundCategory.BLOCKS, 1f, 1f);
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.translatable("tooltip.mccourse.sound_block"));
+        //super.appendTooltip(stack, world, tooltip, options);
     }
 }
--- src/main/java/net/kaupenjoe/mccourse/item/custom/MetalDetectorItem.java
+++ src/main/java/net/kaupenjoe/mccourse/item/custom/MetalDetectorItem.java
@@ -3,8 +3,11 @@ package net.kaupenjoe.mccourse.item.custom;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
@@ -13,9 +16,12 @@ import net.minecraft.text.TextColor;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Colors;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 import java.text.NumberFormat;
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     //private NumberFormat stringFormatter = NumberFormat.getInstance();
@@ -52,6 +58,17 @@ public class MetalDetectorItem extends Item {
 
         //return super.useOnBlock(context);
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()){
+            tooltip.add(Text.translatable("tooltip.mccourse.metal_detector.tooltip.shift"));
+        } else {
+            tooltip.add(Text.translatable("tooltip.mccourse.metal_detector.tooltip"));
+        }
+
+        //super.appendTooltip(stack, world, tooltip, context);
     }
 
     private void outputValuableCoordinates(BlockPos position, PlayerEntity player, Block block) {
